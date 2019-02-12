20f295b9daf5c7fab4383c59451b9a53877c260b
John Casey <jdcasey@commonjava.org>
2019-02-11 23:29:48 -0600

    improve validation of ExpectationServer and also logging output for debugging.

--- src/main/java/org/commonjava/test/http/expect/ExpectationServer.java
+++ src/main/java/org/commonjava/test/http/expect/ExpectationServer.java
@@ -39,6 +39,8 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static org.junit.Assert.fail;
+
 public class ExpectationServer
         extends ExternalResource
         implements HttpServerFixture<ExpectationServer>
@@ -86,6 +88,7 @@ public class ExpectationServer
     public void before()
             throws Exception
     {
+        logger.info( "Starting ExpectationServer" );
         start();
     }
 
@@ -105,6 +108,7 @@ public class ExpectationServer
                                                       .setClassLoader( Thread.currentThread().getContextClassLoader() );
 
         final DeploymentManager dm = Servlets.defaultContainer().addDeployment( di );
+        logger.info( "Setting up servlet deployment" );
         dm.deploy();
 
         final AtomicReference<Integer> foundPort = new AtomicReference<>();
@@ -112,8 +116,12 @@ public class ExpectationServer
             foundPort.set( Integer.valueOf( p ) );
             try
             {
+                logger.info( "Got port: {}", p );
                 Undertow s = Undertow.builder().setHandler( dm.start() ).addHttpListener( p, "127.0.0.1" ).build();
+
+                logger.info( "Undertow server is setup and ready to start" );
                 s.start();
+                logger.info( "Undertow started; returning to outer start method" );
 
                 return s;
             }
@@ -125,13 +133,14 @@ public class ExpectationServer
 
         this.port = foundPort.get();
 
-        logger.info( "STARTED Test HTTP Server on 127.0.0.1:" + port );
+        logger.info( "STARTED Test HTTP Server on 127.0.0.1:{}", this.port );
 
         return this;
     }
 
     public String formatUrl( final String... subpath )
     {
+        checkStarted();
         try
         {
             return UrlUtils.buildUrl( "http://127.0.0.1:" + port, servlet.getBaseResource(), subpath );
@@ -144,6 +153,7 @@ public class ExpectationServer
 
     public String formatPath( final String... subpath )
     {
+        checkStarted();
         try
         {
             return UrlUtils.buildPath( servlet.getBaseResource(), subpath );
@@ -156,6 +166,7 @@ public class ExpectationServer
 
     public String getBaseUri()
     {
+        checkStarted();
         try
         {
             return UrlUtils.buildUrl( "http://127.0.0.1:" + port, servlet.getBaseResource() );
@@ -169,44 +180,52 @@ public class ExpectationServer
     public String getUrlPath( final String url )
             throws MalformedURLException
     {
+        checkStarted();
         return new URL( url ).getPath();
     }
 
     public Map<String, Integer> getAccessesByPathKey()
     {
+        checkStarted();
         return servlet.getAccessesByPath();
     }
 
     public Map<String, ContentResponse> getRegisteredErrors()
     {
+        checkStarted();
         return servlet.getRegisteredErrors();
     }
 
     public void registerException( final String url, final String error )
     {
+        checkStarted();
         servlet.registerException( "HEAD", url, 500, error );
         servlet.registerException( "GET", url, 500, error );
     }
 
     public void registerException( final String method, final String url, final String error )
     {
+        checkStarted();
         servlet.registerException( method, url, 500, error );
     }
 
     public void registerException( final String url, final String error, final int responseCode )
     {
+        checkStarted();
         servlet.registerException( "HEAD", url, responseCode, error );
         servlet.registerException( "GET", url, responseCode, error );
     }
 
     public void registerException( final String method, final String url, final int responseCode, final String error )
     {
+        checkStarted();
         servlet.registerException( method, url, responseCode, error );
     }
 
     public void expect( final String testUrl, final int responseCode, final String body )
             throws Exception
     {
+        checkStarted();
         servlet.expect( "GET", testUrl, responseCode, body );
         servlet.expect( "HEAD", testUrl, responseCode, (String) null );
     }
@@ -214,12 +233,14 @@ public class ExpectationServer
     public void expect( final String method, final String testUrl, final int responseCode, final String body )
             throws Exception
     {
+        checkStarted();
         servlet.expect( method, testUrl, responseCode, body );
     }
 
     public void expect( final String testUrl, final int responseCode, final InputStream bodyStream )
             throws Exception
     {
+        checkStarted();
         servlet.expect( "GET", testUrl, responseCode, bodyStream );
         servlet.expect( "HEAD", testUrl, responseCode, (String) null );
     }
@@ -228,32 +249,47 @@ public class ExpectationServer
                         final InputStream bodyStream )
             throws Exception
     {
+        checkStarted();
         servlet.expect( method, testUrl, responseCode, bodyStream );
     }
 
     public void expect( final String method, final String testUrl, ExpectationHandler handler)
             throws Exception
     {
+        checkStarted();
         servlet.expect( method, testUrl, handler );
     }
 
     public String getAccessKey( final CommonMethod method, final String path )
     {
+        checkStarted();
         return servlet.getAccessKey( method, path );
     }
 
     public String getAccessKey( final String method, final String path )
     {
+        checkStarted();
         return servlet.getAccessKey( method, path );
     }
 
     public Integer getAccessesFor( final String path )
     {
+        checkStarted();
         return servlet.getAccessesFor( path );
     }
 
     public Integer getAccessesFor( final String method, final String path )
     {
+        checkStarted();
         return servlet.getAccessesFor( method, path );
     }
+
+    private void checkStarted()
+    {
+        if ( server == null )
+        {
+            fail( "ExpectationServer is NOT started." );
+        }
+    }
+
 }
